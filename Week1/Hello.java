d9ba35f46bee9c633ea1c9a96e7a08201ac691e2
Gauravbudhathoki <budhathokigaurav5@gmail.com>
2024-04-11 10:16:13 +0545

    Made changes

--- Week1/Hello.java
+++ Week1/Hello.java
@@ -3,5 +3,6 @@ package task.Week1;
 public class Hello {
     public static void main(String[] args) {
         /*  Write a program that prints Hello World! to the console. Save the program as Hello.java.*/
+        System.out.println("Hello World!");
     }
 }
