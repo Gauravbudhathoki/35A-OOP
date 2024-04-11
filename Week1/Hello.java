edbcfbeea542932e5c1f9180999fae0365f9c8f1
Gauravbudhathoki <budhathokigaurav5@gmail.com>
2024-04-11 10:11:44 +0545

    week 1 stated

---
+++ Week1/Hello.java
@@ -0,0 +1,7 @@
+package task.Week1;
+
+public class Hello {
+    public static void main(String[] args) {
+        /*  Write a program that prints Hello World! to the console. Save the program as Hello.java.*/
+    }
+}
