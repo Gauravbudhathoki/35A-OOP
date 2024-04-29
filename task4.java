6963acdc948e355af305f1a7d8e9d1215dde16fd
Gauravbudhathoki <158803386+Gauravbudhathoki@users.noreply.github.com>
2024-04-29 22:31:23 +0545

    Add files via upload

---
+++ task4.java
@@ -0,0 +1,10 @@
+package Taskweek1;
+
+public class task4 {
+    public static void main(String[] args) {
+        /*A well-formed Java program has a main method with { and } braces */
+        System.out.println("A well-formed Java program has a main method with { and } braces");
+    }
+
+}
+
