c1ef9bf5e1118d54dcee2800efe6ff4e09463db9
Gauravbudhathoki <158803386+Gauravbudhathoki@users.noreply.github.com>
2024-04-29 22:28:54 +0545

    Add files via upload

---
+++ task3.java
@@ -0,0 +1,13 @@
+package Taskweek1;
+
+public class task3 {
+    public static void main(String[] args) {
+        /* WAP prints the following pattern:
+    *********
+    *********
+    *********
+    ********* */
+        System.out.println("*********\n*********\n*********\n*********");
+        }
+    }
+
